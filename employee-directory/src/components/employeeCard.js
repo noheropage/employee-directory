81cbd8a32163f92384edb4aa27b3cfa1afc5ef52
noheropage <mursepage@gmail.com>
2021-05-29 11:48:15 -0700

    Init react commit

---
+++ employee-directory/src/components/employeeCard.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Card from 'react-bootstrap'
+
+function employeeCard() {
+    return (
+        <Card>
+            Picture:
+            Name: 
+            Email:
+            Phone:
+        </Card>
+    )
+}
