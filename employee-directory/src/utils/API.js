5410630d54bf40c483fff51c529a44497106be7f
noheropage <mursepage@gmail.com>
2021-05-31 17:18:33 -0700

    Troubleshoot github pages deployment

--- employee-directory/src/utils/API.js
+++ employee-directory/src/utils/API.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default {
     getRandomEmployee: function () {
         return axios.get("https://randomuser.me/api/?results=20&nat=us")
